143f8fc630d09d06d3148393dabeb674ec553804
Cr1ms3nRav3n <66265636+Cr1ms3nRav3n@users.noreply.github.com>
2021-10-11 21:57:45 -0500

    Delete lhf.sh

--- scripts/lhf.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-cat output/nmaprnd1.txt | grep -w "Discovered open port 80" | cut -d ' ' -f 6 > output/httphosts.txt
-
-cat output/nmaprnd1.txt | grep -w "Discovered open port 443" | cut -d ' ' -f 6 > output/httpshosts.txt
-
-cat output/nmaprnd1.txt | grep -w "Discovered open port 445" | cut -d ' ' -f 6 > output/smbhosts.txt
