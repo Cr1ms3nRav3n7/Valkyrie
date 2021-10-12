fcc0b5cc316c0c2cba044bf6f3eb4e16956c11bd
Cr1ms3nRav3n <66265636+Cr1ms3nRav3n@users.noreply.github.com>
2021-10-11 21:57:16 -0500

    Update valkyrie.py
    Changed script to use nmap python module instead of calling so many bash scripts, stops at putting out a list of hosts for further nmap scans, for now...

--- valkyrie.py
+++ valkyrie.py
@@ -5,58 +5,122 @@
 
 import sys
 import subprocess
-import getopt
+import nmap
 from termcolor import colored
 from subprocess import call
+from os.path import exists
 
+#define nmap
+nmap = nmap.PortScanner()
+
+#print banner
 b= open ('valk.txt', 'r')
 print(colored(''.join([line for line in b]),'blue')) 
 
-print (colored('Starting rDNS sweeps, this could take a while...','blue'))
-process = subprocess.Popen(['nmap', '-sL', '-R', '192.168.0.0/16'],  stdout=subprocess.PIPE,
-                           universal_newlines=True)
+#Check for exclusions
+answer = input('Are there any exclusions for this? y/n ')
 
-while True:
-    
-    output = process.stdout.readline()
-    with open('output/rdns.txt', 'a') as f:
-      print(output.strip(),file=f)
-      # Do something else
-      return_code = process.poll()
-      if return_code is not None:
-        # Process has finished, read rest of the output 
-        for output in process.stdout.readlines():
-            print(output.strip(),file=f)
-        break
+if answer == 'y':
+	
+	#Get file for exclusions	
+	file = input('Enter the path to the exclusions file: ')
+	file_exists = exists(file)
+	if file_exists == True:
+	
 
-print()
-print(colored('rDNS sweeps done!', 'blue'))
-print()
-print(colored('Grepping output for live hosts...', 'blue'))
+		print (colored('Starting rDNS sweeps, this could take a while...','blue'))
+		
+		args = "-sL -R --excludefile {}".format(file)
+		
+		nmap.scan(hosts='192.168.2.0/24', arguments=args)
+		f = open('output/hosts.txt', "w")
+		g = open('output/rdns.txt', 'w')
+		for host in nmap.all_hosts():
+			hostname = nmap[host].hostname()
+			if hostname != '':
+				print (host, file=f)
+				print (host, nmap[host].hostname(), file=g)
+		f.close()
+		g.close()
 
-call("scripts/grep.sh")
+		print()
+		print(colored('rDNS sweeps done!', 'blue'))
 
-print()
-print(colored('Creating list of subnets to sweep...', 'blue'))
+		print()
+		print(colored('Creating list of subnets to sweep...', 'blue'))
 
-call("scripts/subnet.sh")
+		call("scripts/subnet.sh")
 
-print()
-print(colored('Sweeping enumerated subnets...', 'blue'))
+		print()
+		print(colored('Sweeping enumerated subnets...', 'blue'))
 
-call("scripts/pingsweep.sh")
+		sweep = open('output/subnets.txt', 'r')
+		lines = sweep.readlines()
+		arg2 = "-sn -n -PE --excludefile {}".format(file)
 
-print()
-print(colored('Running first round nmap scans, hold on to your coffee...', 'blue'))
 
-call("scripts/nmap.sh")
+		for line in lines:
+			output = 'output/'+line.strip()+'.txt'
+			f = open(output, 'w')
+			subnet=line.strip()+'.0/24'
+			nmap.scan(hosts=subnet, arguments=arg2)
+			for host in nmap.all_hosts():
+				status = nmap[host].state()
+				if status == 'up':
+					print(host, file=f)
+				
+			
 
-print()
-print(colored('Enumerating HTTP, HTTPS and SMB from initial nmap scans...','blue'))
+		print()
+		print(colored('Initial enumeration done, check output for files to use for further nmap scans!', 'blue'))
+	else:
+		text = "{} does not exist, check your path".format(file)
+		print()
+		print(colored(text, 'red'))
 
-call("scripts/lhf.sh")
+if answer == 'n':
+	print('No exclusions, continuing...')
+	print()
+	print (colored('Starting rDNS sweeps, this could take a while...','blue'))
 
-print()
-print(colored('Checking SMB hosts for signing policy...', 'blue'))
+	nmap.scan(hosts='192.168.2.0/24', arguments='-sL -R')
+	f = open('output/hosts.txt', "w")
+	g = open('output/rdns.txt', 'w')
+	for host in nmap.all_hosts():
+		hostname = nmap[host].hostname()
+		if hostname != '':
+			print (host, file=f)
+			print (host, nmap[host].hostname(), file=g)
+	f.close()
+	g.close()
 
-call("scripts/smb.sh")
+	print()
+	print(colored('rDNS sweeps done!', 'blue'))
+
+	print()
+	print(colored('Creating list of subnets to sweep...', 'blue'))
+
+	call("scripts/subnet.sh")
+
+	print()
+	print(colored('Sweeping enumerated subnets...', 'blue'))
+
+	sweep = open('output/subnets.txt', 'r')
+	lines = sweep.readlines()
+
+
+	for line in lines:
+		file = 'output/'+line.strip()+'.txt'
+		f = open(file, 'w')
+		subnet=line.strip()+'.0/24'
+		nmap.scan(hosts=subnet, arguments='-sn -n -PE')
+		for host in nmap.all_hosts():
+			status = nmap[host].state()
+			if status == 'up':
+				print(host, file=f)
+elif answer != 'y' or answer != 'n' and file == '':
+	print()
+	print(colored('Invalid argument, please use y or n', 'red'))
+
+
+
